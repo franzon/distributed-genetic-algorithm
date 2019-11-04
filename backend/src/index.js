554951cd6b66c00a950a987024ec24d25ccfbad4
Jorge Rossi <jorgefzrossi@gmail.com>
2019-11-04 20:57:58 -0300

    Atualizando lista das melhores soluções

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,21 +2,30 @@ const express = require('express')
 const cors = require('cors')
 const redis = require('redis')
 const knexConfig = require('../knexfile').development
-
 const knex = require('knex')(knexConfig);
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-
 const subscriber = redis.createClient()
 
-subscriber.on("message", (channel, message) => {
+subscriber.on("message", async (channel, message) => {
     try {
+        const data = JSON.parse(message)
+        const taskId = Number.parseInt(channel.split("_")[1])
+        if (data.status === "completed") {
 
+        } else {
+            const solutions = await knex('solutions').select('id', 'user_name', 'genome', 'fitness').where({ task_id: taskId }).orderBy('fitness', 'desc').limit(20)
+            const worstBestSolution = solutions.pop()
+
+            if (!worstBestSolution || data.fitness > worstBestSolution.fitness) {
+                await knex('solutions').insert({ user_name: data.user_name, genome: data.genome, fitness: data.fitness, task_id: taskId })
+            }
+        }
     } catch (error) {
-
+        console.log(error)
     }
 })
 
